fa458c34ab8ee23dd6bc84114b6666447195684a
Grace G Pascarella <pascargr@bc.edu>
2026-04-13 14:27:59 -0400

    add notification to startup screen

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,20 @@ import {
     JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { Notification } from '@jupyterlab/apputils';
+
 const extension: JupyterFrontEndPlugin<void> = {
     id: 'lyons:main',
     autoStart: true,
     activate: (
         app: JupyterFrontEnd,
     ) => {
-        console.log("Hello world from Lyons!")
+        console.log("testing again") 
+        function createNotification() {
+            Notification.warning("Reminder: Files are for editing, Pubs are not.", {autoClose: 2000})
+        }
+        // timeout needed to allow pop up notification on the screen
+        setTimeout(createNotification, 1000)
     }
 }
 export default extension;
